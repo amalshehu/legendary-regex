d62ba3a257137895021ce57e378aaf0f05e07e6f
Amal Shehu <amalshehu@gmail.com>
2016-09-09 23:46:32 +0530

    Print items in matches variable

--- group_capture.py
+++ group_capture.py
@@ -2,3 +2,9 @@ import re
 # Lets use a regular expression to match a few date strings.
 regex = r"[a-zA-Z]+ \d+"
 matches = re.findall(regex, "July 16, September 09, December 12")
+for match in matches:
+    # This will print:
+    #   July 16
+    #   September 09
+    #   December 12
+    print "Full match: %s" % (match)
