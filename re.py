6109b4618d81d3ecda322f6ef33df6cb5aff6a26
Amal Shehu <amalshehu@gmail.com>
2016-09-09 23:19:26 +0530

    set match variable

--- re.py
+++ re.py
@@ -4,3 +4,7 @@ import re
 regex = r"([a-zA-Z]+) (\d+)"
 if re.search(regex, "September 9"):
     # Indeed, the expression "([a-zA-Z]+) (\d+)" matches the date string
+     # If we want, we can use the MatchObject's start() and end() methods
+    # to retrieve where the pattern matches in the input string, and the
+    # group() method to get all the matches and captured groups.
+    match = re.search(regex, "September 9")    
