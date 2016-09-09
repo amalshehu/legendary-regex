a00afc6bd5cb528527fe7f806b44ad806857d185
Amal Shehu <amalshehu@gmail.com>
2016-09-09 23:49:20 +0530

    Print months using re.findall()

--- group_capture.py
+++ group_capture.py
@@ -8,3 +8,12 @@ for match in matches:
     #   September 09
     #   December 12
     print "Full match: %s" % (match)
+# To capture the specific months of each date we can use the following pattern
+regex = r"([a-zA-Z]+) \d+"
+matches = re.findall(regex, "July 16, September 09, December 12")
+for match in matches:
+    # This will now print:
+    #   June
+    #   August
+    #   Dec
+    print "Match month: %s" % (match)
