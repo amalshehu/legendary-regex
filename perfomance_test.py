71605e562b226a7429709edeeb9001356766031c
Amal Shehu <amalshehu@gmail.com>
2016-09-10 20:30:56 +0530

    Print condition perfomance

--- perfomance_test.py
+++ perfomance_test.py
@@ -6,9 +6,16 @@ input = "maximum"
 if "x" in input:
     print("Non-RE")
 
-print time.clock()
+t1 = time.clock()
+print (t1)
 
 if re.search("x", input):
     print("RE")
 
-print time.clock()
+t2 = time.clock()
+print(t2)
+
+if(t1 < t2):
+    print "Non-RE is faster."
+else:
+    print ("RE is faster.")
