f47ec647a126d72da128aa69b408cd5473ba2ff4
Amal Shehu <amalshehu@gmail.com>
2016-09-10 20:45:32 +0530

    Add perfomance TEST #2

--- perfomance_test.py
+++ perfomance_test.py
@@ -19,3 +19,30 @@ if(t1 < t2):
     print "Non-RE is faster."
 else:
     print ("RE is faster.")
+
+print("############### TEST 2 ################")
+# V1: in.
+c = 0
+i = 0
+while i < 1000000:
+    if "x" in input:
+        c += 1
+    i += 1
+
+print("Non-RE")
+t3 = time.clock()
+print (t3)
+# V2: re.search.
+i = 0
+while i < 1000000:
+    if re.search("x", input):
+        c += 1
+    i += 1
+
+print("RE")
+t4 = time.clock()
+print (t4)
+if(t3 < t4):
+    print "Non-RE is faster."
+else:
+    print ("RE is faster.")
